e838b3391ef1782d89542e608e758d6669830c55
ikin-dev <ikin-dev@users.noreply.github.com>
2025-10-12 04:15:44 +0300

    Add fetch latest version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,42 @@
 // import iconDark from './assets/fairykey-icon-dark.png'
 import iconLight from '../assets/fairykey-icon-light.png'
-const downloadUrl = "https://github.com/FairyKey/FairyKey/releases/download/v1.0.3/FairyKey-v1.0.3-win-x64.zip"
+import { useEffect, useState } from 'react'
 
 function Hero() {
+    const [release, setRelease] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchLatestRelease() {
+            try {
+                const response = await fetch(
+                    `https://api.github.com/repos/FairyKey/FairyKey/releases/latest`
+                );
+
+                if (!response.ok) {
+                    throw new Error('failed to fetch newest release')
+                }
+
+                const data = await response.json();
+                const zipAsset = data.assets?.find(asset => asset.name.endsWith('.zip'));
+
+                setRelease({
+                    version: data.tag_name,
+                    downloadUrl: zipAsset?.browser_download_url
+                });
+            } catch (err) {
+                setError(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchLatestRelease()
+    }, []);
+
+    const downloadUrl = release?.downloadUrl || "https://github.com/FairyKey/FairyKey/releases"
+    const version = release?.version?.slice(1) || "Latest release" // remove v from start of tag
+
     return (
         <div className="flex flex-col md:flex-row-reverse justify-center items-center ">
             <img
@@ -18,14 +52,19 @@ function Hero() {
                 <div className="flex max-[400px]:flex-col m-5 justify-center items-center flex-row gap-5 max-[400px]:gap-1">
                     <a
                         href={downloadUrl}
-                        className="text-white bg-sky-400 hover:bg-sky-600 focus:ring-blue-300 font-medium rounded-full p-5 cursor-pointer"
-                    >
+                        className="text-white bg-sky-400 hover:bg-sky-600 focus:ring-blue-300 font-medium rounded-full p-5 cursor-pointer">
                         Download
                     </a>
+
                     <div className="flex-col">
-                        {/* <p className="text-gray-500">Latest release</p> */}
-                        <p>Version 1.0.3</p>
-                        <p className="text-gray-400">Windows 64-bit</p>
+                        {loading && <p>Loading...</p>}
+                        {error && <p className="text-red-500">Error: {error}</p>}
+                        {!loading && !error && (
+                            <>
+                                <p>Version {version}</p>
+                                <p className="text-gray-400">Windows 64-bit</p>
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
